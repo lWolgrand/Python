c167bc2f17734bab30e303e3f6366d1b8f828d53
Wolgrand Lopes <lopeswolgrand@gmail.com>
2021-12-08 02:45:16 -0300

    métodos

--- conta.py
+++ conta.py
@@ -7,4 +7,11 @@ class Conta:
         self.saldo = saldo
         self.limite = limite
 
-print(Conta)
+    def extrato(self)
+        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
