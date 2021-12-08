71a7bd20c33cb28078efdb933961772258652d9e
Wolgrand Lopes <lopeswolgrand@gmail.com>
2021-12-08 13:44:17 -0300

    propriedades privadas

--- conta.py
+++ conta.py
@@ -1,17 +1,22 @@
+import teste
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print("constuindo objeto ...{}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
-    def extrato(self)
-        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
+    def extrato(self, valor):
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        self.deposita(valor)
