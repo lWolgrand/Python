3fd65e19adcbdcb493a46c19b53dca4547bc0d72
lWolgrand <67397900+lWolgrand@users.noreply.github.com>
2021-12-13 23:50:02 -0300

    Add files via upload
    Att propriedades

--- oo/conta.py
+++ oo/conta.py
@@ -2,27 +2,34 @@ import teste
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
-        print("constuindo objeto ...{}".format(self))
+        print("Construindo objeto ... {}".format(self))
         self.__numero = numero
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
 
-    def extrato(self, valor):
+    def extrato(self):
         print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
 
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
 
     @property
-    def get_saldo(self):
+    def saldo(self):
         return self.__saldo
 
     @property
@@ -30,10 +37,17 @@ class Conta:
         return self.__titular
 
     @property
-    def get_limite(self):
+    def limite(self):
         return self.__limite
 
     @limite.setter
-    def set_limite(self, limite):
+    def limite(self, limite):
         self.__limite = limite
-        
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
